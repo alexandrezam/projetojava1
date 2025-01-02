1ea3b4bf1ece148057a7e87599eda29b5d79c9e6
alexa <alexa@192.168.0.9>
2025-01-02 21:53:56 -0200

    Alteracao adicional

--- projetojava1/src/curso/java/alex/pessoa.java
+++ projetojava1/src/curso/java/alex/pessoa.java
@@ -2,4 +2,20 @@ package curso.java.alex;
 
 public class pessoa {
 
+	private string Nome;
+	private string Sobrenome;
+	public string getNome() {
+		return Nome;
+	}
+	public void setNome(string nome) {
+		Nome = nome;
+	}
+	public string getSobrenome() {
+		return Sobrenome;
+	}
+	public void setSobrenome(string sobrenome) {
+		Sobrenome = sobrenome;
+	}
+	
+	
 }
