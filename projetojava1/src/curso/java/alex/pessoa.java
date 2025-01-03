700840566589af3a93d8302aaecddcf5eff14e7b
alexa <alexa@192.168.0.9>
2025-01-02 22:05:37 -0200

    adicionando o campo de idade na classe pessoa

--- projetojava1/src/curso/java/alex/pessoa.java
+++ projetojava1/src/curso/java/alex/pessoa.java
@@ -4,6 +4,15 @@ public class pessoa {
 
 	private string Nome;
 	private string Sobrenome;
+	
+	private int Idade;
+	
+	public int getIdade() {
+		return Idade;
+	}
+	public void setIdade(int idade) {
+		Idade = idade;
+	}
 	public string getNome() {
 		return Nome;
 	}
